f2bf75f08d4dd790e8d948d8679e58c699ab0899
raissa <raissaasouzaa@icloud.com>
2024-08-08 21:56:58 -0300

    atualizado

--- obj/Debug/net8.0/lab01-conhecendo-o-ambiente-de-desenvolvimento.AssemblyInfo.cs
+++ obj/Debug/net8.0/lab01-conhecendo-o-ambiente-de-desenvolvimento.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab01-conhecendo-o-ambiente-de-desenvolvimento")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47cf536cbace8a03ce9501553603719e2a12a567")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab01-conhecendo-o-ambiente-de-desenvolvimento")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab01-conhecendo-o-ambiente-de-desenvolvimento")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
